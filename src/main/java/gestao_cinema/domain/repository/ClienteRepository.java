d5d9455f569aaaa2a920cac709e2cb3aa267a81c
Kaua Felix da Silva Costa <kfelixcosta11@gmail.com>
2025-12-10 19:34:26 -0300

    feat: implementa interface ClienteRepository e sua implementação

--- src/main/java/gestao_cinema/domain/repository/ClienteRepository.java
+++ src/main/java/gestao_cinema/domain/repository/ClienteRepository.java
@@ -1,4 +1,16 @@
 package gestao_cinema.domain.repository;
 
+import gestao_cinema.domain.model.Cliente;
+
 public interface ClienteRepository {
+
+    public void criarCliente(Cliente cliente);
+
+    public Cliente buscarClientePorId(Long id);
+
+    public void atualizarCliente(Cliente cliente);
+
+    public void deletarCliente(Long id);
+
+    public Cliente buscarClientePorNome(String nome);
 }
--- src/main/java/gestao_cinema/infrastructure/persistence/ClienteRepositoryImpl.java
+++ src/main/java/gestao_cinema/infrastructure/persistence/ClienteRepositoryImpl.java
@@ -1,4 +1,32 @@
 package gestao_cinema.infrastructure.persistence;
 
-public class ClienteRepositoryImpl {
+import gestao_cinema.domain.model.Cliente;
+import gestao_cinema.domain.repository.ClienteRepository;
+
+public class ClienteRepositoryImpl implements ClienteRepository {
+
+    @Override
+    public void criarCliente(Cliente cliente) {
+
+    }
+
+    @Override
+    public Cliente buscarClientePorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public void atualizarCliente(Cliente cliente) {
+
+    }
+
+    @Override
+    public void deletarCliente(Long id) {
+
+    }
+
+    @Override
+    public Cliente buscarClientePorNome(String nome) {
+        return null;
+    }
 }
