d7cd2cc5e29899da1fd7aa4c13d2fdb131cd0b7f
Kaua Felix da Silva Costa <kfelixcosta11@gmail.com>
2025-12-10 19:35:12 -0300

    feat: implementa metodos da interface FilmeRepository na classe de implementacao FilmeRepositoryImpl

--- src/main/java/gestao_cinema/domain/repository/FilmeRepository.java
+++ src/main/java/gestao_cinema/domain/repository/FilmeRepository.java
@@ -1,4 +1,13 @@
 package gestao_cinema.domain.repository;
 
+import gestao_cinema.domain.model.Filme;
+
 public interface FilmeRepository {
+
+    public void criarFilme(Filme filme);
+
+    public Filme buscarFilmePorGenero(long ig);
+
+
+
 }
--- src/main/java/gestao_cinema/infrastructure/persistence/FilmeRepositoryImpl.java
+++ src/main/java/gestao_cinema/infrastructure/persistence/FilmeRepositoryImpl.java
@@ -1,4 +1,17 @@
 package gestao_cinema.infrastructure.persistence;
 
-public class FilmeRepositoryImpl {
+import gestao_cinema.domain.model.Filme;
+import gestao_cinema.domain.repository.FilmeRepository;
+
+public class FilmeRepositoryImpl implements FilmeRepository {
+
+    @Override
+    public void criarFilme(Filme filme) {
+
+    }
+
+    @Override
+    public Filme buscarFilmePorGenero(long ig) {
+        return null;
+    }
 }
