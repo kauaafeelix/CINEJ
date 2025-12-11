e369873e2fb3de246c798cff791c81b803800de2
Kauã Felix <kfelixcosta11@gmail.com>
2025-12-11 11:36:01 -0300

    feat: implementa metodos de criar um Cliente, buscar cliente por ID e por nome com uso do LIKE para faciitar a busca

--- src/main/java/gestao_cinema/infrastructure/persistence/FilmeRepositoryImpl.java
+++ src/main/java/gestao_cinema/infrastructure/persistence/FilmeRepositoryImpl.java
@@ -3,15 +3,18 @@ package gestao_cinema.infrastructure.persistence;
 import gestao_cinema.domain.model.Filme;
 import gestao_cinema.domain.repository.FilmeRepository;
 
+import java.sql.SQLException;
+import java.util.List;
+
 public class FilmeRepositoryImpl implements FilmeRepository {
 
     @Override
-    public void criarFilme(Filme filme) {
+    public void criarFilme(Filme filme)  throws SQLException {
 
     }
 
     @Override
-    public Filme buscarFilmePorGenero(long ig) {
+    public List<Filme> buscarFilmePorGenero(String genero) throws SQLException {
         return null;
     }
 }
