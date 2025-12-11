4356ae94d0ad4687f8caf239de5f5f7e5be4b69e
Kauã Felix <kfelixcosta11@gmail.com>
2025-12-11 16:45:38 -0300

    feat: adiciona novos atributos e métodos ao EmitirIngressoDTO para melhor gerenciamento de ingressos

--- src/main/java/gestao_cinema/application/dto/ingresso/EmitirIngressoDTO.java
+++ src/main/java/gestao_cinema/application/dto/ingresso/EmitirIngressoDTO.java
@@ -3,39 +3,86 @@ package gestao_cinema.application.dto.ingresso;
 import gestao_cinema.domain.model.Cliente;
 import gestao_cinema.domain.model.Filme;
 import gestao_cinema.domain.model.Sessao;
+import gestao_cinema.domain.model.enums.StatusIngresso;
+import gestao_cinema.domain.model.enums.TipoIngresso;
+
+import java.util.Date;
 
 public class EmitirIngressoDTO {
-    private Cliente cliente;
-    private Sessao sessao;
-    private Filme filme;
+    private long id;
+    private String clienteNome;
+    private String salaSessao;
+    private String filmeTitulo;
+    private Date dataCompra;
+    private TipoIngresso tipoIngresso;
+    private StatusIngresso status;
 
-    public EmitirIngressoDTO(Cliente cliente, Sessao sessao, Filme filme) {
-        this.cliente = cliente;
-        this.sessao = sessao;
-        this.filme = filme;
+    public EmitirIngressoDTO(Long id, String clienteNome, String salaSessao, String filmeTitulo, Date dataCompra, TipoIngresso tipoIngresso, StatusIngresso status) {
+        this.id = id;
+        this.clienteNome = clienteNome;
+        this.salaSessao = salaSessao;
+        this.filmeTitulo = filmeTitulo;
+        this.dataCompra = dataCompra;
+        this.tipoIngresso = tipoIngresso;
+        this.status = status;
     }
 
-    public Cliente getCliente() {
-        return cliente;
+
+    public long getId() {
+        return id;
     }
 
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
+    public void setId(long id) {
+        this.id = id;
     }
 
-    public Sessao getSessao() {
-        return sessao;
+    public String getClienteNome() {
+        return clienteNome;
     }
 
-    public void setSessao(Sessao sessao) {
-        this.sessao = sessao;
+    public void setClienteNome(String clienteNome) {
+        this.clienteNome = clienteNome;
     }
 
-    public Filme getFilme() {
-        return filme;
+    public String getSalaSessao() {
+        return salaSessao;
     }
 
-    public void setFilme(Filme filme) {
-        this.filme = filme;
+    public void setSalaSessao(String salaSessao) {
+        this.salaSessao = salaSessao;
+    }
+
+    public String getFilmeTitulo() {
+        return filmeTitulo;
+    }
+
+    public void setFilmeTitulo(String filmeTitulo) {
+        this.filmeTitulo = filmeTitulo;
+    }
+
+    public Date getDataCompra() {
+        return dataCompra;
+    }
+
+    public void setDataCompra(Date dataCompra) {
+        this.dataCompra = dataCompra;
+    }
+
+    public TipoIngresso getTipoIngresso() {
+        return tipoIngresso;
+    }
+
+    public void setTipoIngresso(TipoIngresso tipoIngresso) {
+        this.tipoIngresso = tipoIngresso;
+    }
+
+    public StatusIngresso getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusIngresso status) {
+        this.status = status;
     }
 }
+
+
