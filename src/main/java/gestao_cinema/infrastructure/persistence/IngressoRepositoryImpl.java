4475efdf162963dda47bdad16df3ea4e0b67b5c1
Kauã Felix <kfelixcosta11@gmail.com>
2025-12-11 11:36:48 -0300

    refactor: implementa os metodos do repository que foram alterados

--- src/main/java/gestao_cinema/infrastructure/persistence/IngressoRepositoryImpl.java
+++ src/main/java/gestao_cinema/infrastructure/persistence/IngressoRepositoryImpl.java
@@ -1,6 +1,25 @@
 package gestao_cinema.infrastructure.persistence;
 
+import gestao_cinema.domain.model.Ingresso;
+import gestao_cinema.domain.model.enums.StatusIngresso;
 import gestao_cinema.domain.repository.IngressoRepository;
 
+import java.sql.SQLException;
+import java.util.List;
+
 public class IngressoRepositoryImpl implements IngressoRepository {
+    @Override
+    public void criarIngresso(Ingresso ingresso)  throws SQLException {
+
+    }
+
+    @Override
+    public Ingresso alterarStatus(StatusIngresso status)  throws SQLException{
+        return null;
+    }
+
+    @Override
+    public List<Ingresso> listarTodosIngressosPorCliente()  throws SQLException{
+        return List.of();
+    }
 }
