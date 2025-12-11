5e3704139676f481696cfb0de884061a92eeccd8
Kauã Felix <kfelixcosta11@gmail.com>
2025-12-11 16:28:29 -0300

    feat: implementa metodos do repository na classe de implementacao SessaoRepositoryImpl

--- src/main/java/gestao_cinema/infrastructure/persistence/SessaoRepositoryImpl.java
+++ src/main/java/gestao_cinema/infrastructure/persistence/SessaoRepositoryImpl.java
@@ -1,25 +1,23 @@
 package gestao_cinema.infrastructure.persistence;
 
 import gestao_cinema.domain.model.Ingresso;
+import gestao_cinema.domain.model.Sessao;
 import gestao_cinema.domain.model.enums.StatusIngresso;
 import gestao_cinema.domain.repository.IngressoRepository;
+import gestao_cinema.domain.repository.SessaoRepository;
 
 import java.sql.SQLException;
 import java.util.List;
 
-public class SessaoRepositoryImpl implements IngressoRepository {
+public class SessaoRepositoryImpl implements SessaoRepository {
+
     @Override
-    public void criarIngresso(Ingresso ingresso)  throws SQLException {
+    public void criarSessao(Sessao sessao) throws SQLException {
 
     }
 
     @Override
-    public Ingresso alterarStatus(StatusIngresso status)  throws SQLException{
-        return null;
-    }
-
-    @Override
-    public List<Ingresso> listarTodosIngressosPorCliente()  throws SQLException{
+    public List<Sessao> procurarSessaoPorFilme() throws SQLException {
         return List.of();
     }
 }
