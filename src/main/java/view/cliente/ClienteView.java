925055b37768dd18c1f3436a655f3c87f750176b
kauaafeelix <kfelixcosta11@mail.com>
2025-12-12 16:42:37 -0300

    feat: inicia interacao com usuario para CRUD de clientes

---
+++ src/main/java/view/cliente/ClienteView.java
@@ -0,0 +1,50 @@
+package view.cliente;
+
+import gestao_cinema.domain.model.Cliente;
+
+import java.util.Scanner;
+
+public class ClienteView {
+
+    Scanner scNum = new Scanner (System.in);
+    Scanner scStr = new Scanner(System.in);
+
+
+    public int menuCliente (){
+        System.out.println("""
+                1 - Cadastrar Cliente
+                2 - Listar Clientes
+                3 - Atualizar um Cliente
+                4 - Deletar um Cliente
+                
+                """);
+
+        int opcao = capturarOpcao();
+
+        return opcao;
+    }
+
+    public int capturarOpcao(){
+        System.out.println("Digite sua Opção: ");
+        return scNum.nextInt();
+    }
+
+    public Cliente cadastrarCliente(){
+        System.out.println("""
+                ==== CADASTRO DE CLIENTE ====
+                """);
+        System.out.println("Digite o Nome do Cliente: ");
+        String nome = scStr.nextLine();
+
+        System.out.println("Digite o Email do Cliente: ");
+        String email = scStr.nextLine();
+
+        System.out.println("Digite o Saldo da Carteira do Cliente: ");
+        double saldo = scNum.nextDouble();
+
+        Cliente novoCliente = new Cliente(nome, email, saldo);
+
+        return novoCliente;
+
+    }
+}
