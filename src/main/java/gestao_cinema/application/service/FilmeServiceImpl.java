834479263afd7254815c606fc4eac62b80d3fd8d
Kaua Felix <kfelixcosta11@gmail.com>
2026-02-03 18:43:33 -0300

    feat: adiciona metodo para cadastrar um filme

--- src/main/java/gestao_cinema/application/service/FilmeServiceImpl.java
+++ src/main/java/gestao_cinema/application/service/FilmeServiceImpl.java
@@ -1,4 +1,31 @@
 package gestao_cinema.application.service;
 
+import gestao_cinema.domain.model.Filme;
+import gestao_cinema.domain.repository.FilmeRepository;
+import gestao_cinema.infrastructure.persistence.FilmeRepositoryImpl;
+import gestao_cinema.view.filme.FilmeView;
+
 public class FilmeServiceImpl {
+
+    FilmeView filmeView = new FilmeView();
+    FilmeRepositoryImpl filmeRepository = new FilmeRepositoryImpl();
+
+    public void cadastrarFilme(){
+        var filme = new Filme();
+        filme = filmeView.cadastrarFilmes();
+
+        if (filme.getTitulo() == null || filme.getGenero() == null || filme.getDuracaoMinutos() <= 0){
+            System.out.println("Dados inválidos! Por favor, tente novamente.");
+            return;
+        } else {
+            try {
+                filmeRepository.criarFilme(filme);
+                System.out.println("Filme cadastrado com sucesso!");
+            } catch (Exception e) {
+                System.out.println("Erro ao cadastrar o filme: " );
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
